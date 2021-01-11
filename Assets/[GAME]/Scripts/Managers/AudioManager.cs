a2501c0181757c6fc38f235f562ff2d170f2c745
AhmetBalikcioglu <69127340+AhmetBalikcioglu@users.noreply.github.com>
2021-01-12 02:04:56 +0300

    +add AudioManager

---
+++ Assets/[GAME]/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using UnityEngine.Audio;
+using UnityEngine;
+
+public class AudioManager : Singleton<AudioManager>
+{
+    public Sound[] sounds;
+
+    void Awake()
+    {
+        if (AudioManager.Instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+    private void OnEnable()
+    {
+        if (Managers.Instance == null)
+            return;
+    }
+
+    private void OnDisable()
+    {
+        if (Managers.Instance == null)
+            return;
+    }
+
+    void Start()
+    {
+        Play("Music");
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
+        s.source.Play();
+    }
+
+    public void ChangeVolume(string name, bool isOn)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
+        if (isOn)
+        {
+            s.source.volume = 0f;
+        }
+        else
+        {
+            s.source.volume = 0.05f;
+        }
+    }
+}
